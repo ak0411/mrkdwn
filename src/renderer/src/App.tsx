226ec64fe0e6b1f6c2f5261e5ec19668c0fa45b0
Anawil Karavek <akaravek@hotmail.com>
2024-08-17 15:52:39 +0700

    added state management with jotai

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -8,17 +8,24 @@ import {
   RootLayout,
   Sidebar
 } from '@/components'
+import { useRef } from 'react'
 
 const App = () => {
+  const contentContainerRef = useRef<HTMLDivElement>(null)
+
+  const resetScroll = () => {
+    contentContainerRef.current?.scrollTo(0, 0)
+  }
+
   return (
     <>
       <DraggableTopBar />
       <RootLayout>
         <Sidebar className="p-2">
           <ActionButtonRows className="flex justify-between mt-1" />
-          <NotePreviewList className="mt-3 space-y-1" />
+          <NotePreviewList className="mt-3 space-y-1" onSelect={resetScroll} />
         </Sidebar>
-        <Content className="bg-zinc-900/50">
+        <Content ref={contentContainerRef} className="bg-zinc-900/50">
           <FloatingNoteTitle className="pt-2" />
           <MarkdownEditor />
         </Content>
