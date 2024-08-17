868564d184299d692f895a48d9b91e393363cc7d
Anawil Karavek <akaravek@hotmail.com>
2024-08-17 15:23:30 +0700

    added markdown support

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,6 +2,8 @@ import {
   ActionButtonRows,
   Content,
   DraggableTopBar,
+  FloatingNoteTitle,
+  MarkdownEditor,
   NotePreviewList,
   RootLayout,
   Sidebar
@@ -16,7 +18,10 @@ const App = () => {
           <ActionButtonRows className="flex justify-between mt-1" />
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="bg-zinc-900/50">Content</Content>
+        <Content className="bg-zinc-900/50">
+          <FloatingNoteTitle className="pt-2" />
+          <MarkdownEditor />
+        </Content>
       </RootLayout>
     </>
   )
