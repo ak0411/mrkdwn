b53f0369d56376728dfade0fb0c0f8e47e0e50ac
Anawil Karavek <akaravek@hotmail.com>
2024-08-22 14:33:04 +0700

    refactored functions in TrafficLights

--- src/renderer/src/components/DraggableTopBar.tsx
+++ src/renderer/src/components/DraggableTopBar.tsx
@@ -1,9 +1,22 @@
+import { usePlatform } from '@renderer/hooks/usePlatform'
 import { TrafficLights } from './TrafficLights'
 
 export const DraggableTopBar = () => {
+  const { isMacOS } = usePlatform()
+  if (isMacOS) return <header className="absolute inset-0 h-8 bg-transparent" />
+
+  const handleClose = () => window.context.close()
+  const handleMinimize = () => window.context.minimize()
+  const handleMaximize = () => window.context.maximize()
+
   return (
     <header className="absolute inset-0 h-8 bg-transparent">
-      <TrafficLights className="m-2" />
+      <TrafficLights
+        onClose={handleClose}
+        onMinimize={handleMinimize}
+        onMaximize={handleMaximize}
+        className="m-2"
+      />
     </header>
   )
 }
--- src/renderer/src/components/TrafficLights.tsx
+++ src/renderer/src/components/TrafficLights.tsx
@@ -1,27 +1,31 @@
-import { usePlatform } from '@renderer/hooks/usePlatform'
 import { ComponentProps } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-export const TrafficLights = ({ className, ...props }: ComponentProps<'div'>) => {
-  const { isMacOS } = usePlatform()
-  if (isMacOS) return null
+type TrafficLightsProps = ComponentProps<'div'> & {
+  onClose?: () => void
+  onMinimize?: () => void
+  onMaximize?: () => void
+}
 
-  const handleClose = () => window.context.close()
-  const handleMinimize = () => window.context.minimize()
-  const handleMaximize = () => window.context.maximize()
-
+export const TrafficLights = ({
+  onClose,
+  onMinimize,
+  onMaximize,
+  className,
+  ...props
+}: TrafficLightsProps) => {
   return (
     <div className={twMerge('flex items-center space-x-2', className)} {...props}>
       <button
-        onClick={handleClose}
+        onClick={onClose}
         className="w-4 h-4 rounded-full bg-red-500 hover:bg-red-600 transition-colors"
       />
       <button
-        onClick={handleMinimize}
+        onClick={onMinimize}
         className="w-4 h-4 rounded-full bg-yellow-500 hover:bg-yellow-600 transition-colors"
       />
       <button
-        onClick={handleMaximize}
+        onClick={onMaximize}
         className="w-4 h-4 rounded-full bg-green-500 hover:bg-green-600 transition-colors"
       />
     </div>
